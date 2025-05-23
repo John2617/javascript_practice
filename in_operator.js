04e2e214388b0c8e0bf5092aeda0ba49e23da5b6
John2617 <134540804+John2617@users.noreply.github.com>
2025-05-23 12:50:36 +0530

    Update in_operator.js

--- in_operator.js
+++ in_operator.js
@@ -1,19 +1,1 @@
-let bio={
-    name:"john",
-    age:21,
-    dob:2004
-}
 
-//" in " operator is used to check whether the key is present in object..it returns boolean result
-console.log('name' in bio) //key should be in string //returns true
-console.log('spec' in bio) //retuns false
-
-// how to display all the elements within the object...we use "For loop with 'in' operator"
-for(key in bio){
-    console.log(bio[key]) // it returns all the values in the objects
-     // it returns only the key names
-}
-// to display only keys
-for (key in bio){
-console.log(key)
-}
